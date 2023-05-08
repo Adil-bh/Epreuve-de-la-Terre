34028bdd68b03e759899741c93e64ef5da99b34e
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-05-09 00:03:54 +0200

    Ajustement L'alphabet à partir de

--- terre03.rb
+++ terre03.rb
@@ -1,19 +1,20 @@
 =begin
+
 1* Je récupère la lettre en argument.
 2* Je mets la lettre dans la variable lettre_de_depart
-3* Je créé un tableau de l'alphabet allant de 0..25
-4* Parcourir le tableau et comparer la lettre avec chaque index
-5* Si lettre_de_départ = lettre_tableau_alphabet on récupère l'index pour le mettre dans index_depart
+3* Je créé un tableau de l'alphabet allant de 0..25 (26 lettres)
+4* Parcourir le tableau alphabet et comparer chaque chaque lettre avec la lettre_de_depart 
+5* Si lettre_de_départ = lettre_tableau_alphabet on récupère l'index de la lettre en question pour le mettre dans index_depart
 6* Créer un nouveau tableau alphabet_tronque allant de index_depart..25
 7* Print chaque élément du tableau alphabet_tronque
 
-	
 =end
+
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 lettre_de_depart = ARGV
 
-for lettre in alphabet #On parcours le tableau alphabet pour comparer la lettre donnée en ARG pour récupérer l'index
-	if lettre == lettre_de_depart[0]
+for lettre in alphabet #On parcours le tableau alphabet pour comparer la lettre donnée en argument pour récupérer l'index
+	if lettre == lettre_de_depart[0] #lettre_de_depart[Ø] car ARGV est un tableau
 		index_depart = alphabet.index(lettre)
 		alphabet_tronque = alphabet[index_depart..25]
 	end
