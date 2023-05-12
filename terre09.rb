7a7e6a5a3f5a270de3d3fd5295d854ef3256889c
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2023-05-12 13:17:40 +0200

    Racine carrée d’un nombre

--- terre09.rb
+++ terre09.rb
@@ -1,11 +1,12 @@
 arguments = ARGV
 nombre = ARGV[0]
-exposant = 2
+exposant = 0.5
 
-
+#Racine carrée = x^1/2
+#Interrogation : Faut il que la sortie soit un entier ou un float ?
 if nombre == nil || arguments.length != 1 || nombre.to_s.count("a-zA-Z") > 0
 	puts "Erreur"
 else
 	resultat = nombre.to_i**exposant
-	puts resultat
+	puts resultat.to_i
 end
